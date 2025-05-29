5407853b1479f9a7c2139f6074c8e73abcfcd3ea
hamsu12 <hamsj2413@smail.kongju.ac.kr>
2025-05-29 18:40:13 +0900

    feat: 가게 리뷰 출력 및 예약 화면 구현

--- src/components/main/PlaceList.js
+++ src/components/main/PlaceList.js
@@ -4,21 +4,51 @@ const placeDataList = [
     {
         title: '스타벅스',
         description: '커피 맛있음',
-        reviews: 60,
+        reviews: [
+            {
+                username: "민수",
+                text: "커피가 맛있고 분위기가 좋아요!"
+            },
+            {
+                username: "지은",
+                text: "의자 편하고 직원 친절했어요."
+            }
+        ],
+
         address: '두정동 어딘가',
         hours: '09:00 ~ 22:00'
     },
     {
         title: '이디야',
         description: '가성비 카페',
-        reviews: 24,
+        reviews: [
+            {
+                username: "민수",
+                text: "커피가 맛있고 분위기가 좋아요!"
+            },
+            {
+                username: "지은",
+                text: "의자 편하고 직원 친절했어요."
+            }
+        ],
+
         address: '신부동 어딘가',
         hours: '08:00 ~ 21:00'
     },
     {
         title: '젤리가게',
         description: '함수픽 젤리맛집',
-        reviews: 3272948,
+        reviews: [
+            {
+                username: "민수",
+                text: "커피가 맛있고 분위기가 좋아요!"
+            },
+            {
+                username: "지은",
+                text: "의자 편하고 직원 친절했어요."
+            }
+        ],
+
         address: '세종 어딘가',
         hours: '11:00 ~ 19:00'
     }
